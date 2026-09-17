505c480928e75d969bbf1b680fcf7fd59bbaab17
sonjehyun123-maker <sonjehyun123@gmail.com>
2026-09-17 21:05:38 +0900

    feat: 날짜 함수 예시값 수정 (conflict 실습용)

--- src/date_utils.py
+++ src/date_utils.py
@@ -16,3 +16,6 @@ def format_date(date_text: str) -> str:
     date = datetime.strptime(date_text, "%Y-%m-%d")
 
     return date.strftime("%Y년 %m월 %d일")
+
+    if __name__ == "__main__":
+        print(format_date("2026-09-17"))
